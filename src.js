696cde23b1d8c7ca9b7a531b30a3bb87ed64c569
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-11-17 18:33:51 -0500

    Approach inicial del sprint

---
+++ src.js
@@ -0,0 +1,31 @@
+rangosAQI = [
+    {etiqueta: 'excelente', de:0, hasta:30},
+    {etiqueta: 'bueno', de:31, hasta:50},
+    {etiqueta: 'moderado', de:51, hasta:100},
+    {etiqueta: 'bajo', de:101, hasta:150},
+    {etiqueta: 'insalubre', de:151, hasta:200},
+    {etiqueta: 'peligroso', de:201, hasta:300},
+]
+
+
+/* const calcularPorcentajes = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) =>{
+    
+} */
+
+const registrarAQI = (value)=>{
+
+    for(let item of rangosAQI){
+
+        return item
+        if(value >= item.de && value<= item.hasta){
+            return item.etiqueta
+        }else{
+            return 'fuera_de_rango'
+        }
+    }
+}
+
+console.log(registrarAQI(90));
+
+/* module.exports.registrarAQI = registrarAQI;
+module.exports.calcularPorcentajes = calcularPorcentajes; */
