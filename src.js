cf6f2159babef4f4e3656040cef50b226c11bda8
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-11-17 18:39:43 -0500

    Se actualiza ranma con develop

--- src.js
+++ src.js
@@ -1,3 +1,12 @@
+rangosAQI = [
+    {etiqueta: 'excelente', de:0, hasta:30},
+    {etiqueta: 'bueno', de:31, hasta:50},
+    {etiqueta: 'moderado', de:51, hasta:100},
+    {etiqueta: 'bajo', de:101, hasta:150},
+    {etiqueta: 'insalubre', de:151, hasta:200},
+    {etiqueta: 'peligroso', de:201, hasta:300},
+];
+
 const calcularPorcentajes = (limitePM10, limitePM2punto5, limiteNO2, limiteCO, limiteS02, valorPM10, valorPM2punto5, valorNO2, valorCO, valorS02) =>{
     
 }
@@ -13,6 +22,8 @@ const registrarAQI = (value)=>{
         }
     }
 }
+
+console.log(registrarAQI(90));
 
 module.exports.registrarAQI = registrarAQI;
 module.exports.calcularPorcentajes = calcularPorcentajes;
