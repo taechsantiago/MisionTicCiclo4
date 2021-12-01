3306b26e5edf4a311a5cfb8228d2b3bf46e64fce
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-11-30 22:23:12 -0500

    Se integra el desarrollo del menú lateral, el header y el mapa

--- ui-web/src/Pages/Dashboard.js
+++ ui-web/src/Pages/Dashboard.js
@@ -1,4 +1,5 @@
 import MediaCard from '../components/Dashboard/MediaCard';
+import Map from '../components/Map';
 
 function Dashboard() {
   return (
@@ -9,12 +10,15 @@ function Dashboard() {
       <section style={{margin:'80px auto', width:'85%', textAlign:'justify', textJustify:'inter-word'}}>
         <p>En Colombia, el monitoreo y control de la contaminación atmosférica ha tomado día a día mayor relevancia, debido a que, según cifras de la Organización Mundial de la Salud, una de cada ocho muertes ocurridas a nivel mundial, es ocasionada por la contaminación del aire. A nivel nacional, el Departamento Nacional de Planeación estimó que, durante el año 2015, los efectos de este fenómeno estuvieron asociados a 10.527 muertes y 67,8 millones de síntomas y enfermedades. Adicionalmente, los costos ambientales asociados a la contaminación atmosférica en Colombia, durante los últimos años se incrementaron pasando de 1,1% del PIB de 2009 ($5,7 billones de pesos) a 1,59% del PIB de 2014 ($12 billones de pesos) y del 1,93% del PIB en 2015 ($15.4 billones de pesos), lo cual pone en evidencia la necesidad de seguir implementando estrategias para controlar, evaluar y monitorear estas sustancias.</p>
       </section>
-      <section style={{display:'flex', flexFlow: 'wrap', width: '700px'}}>
-        <MediaCard />
-        <MediaCard />
-        <MediaCard />
-        <MediaCard />
-      </section>
+      <div style={{display:'flex', flexFlow:'row'}}>
+        <section style={{display:'flex', flexFlow: 'wrap', width: '700px'}}>
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+        </section>
+        <Map/>
+      </div>      
     </div>
   );
 }
--- ui-web/src/components/General/Header.js
+++ ui-web/src/components/General/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
+import '../../styles/Home/header.css';
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <section>
-      <nav>
-        <Link to="/" style={{margin: '3rem', fontSize:'20px'}}>Inicio</Link>
-        <Link to="/indicators" style={{margin: '3rem', fontSize:'20px'}}>Indicadores</Link>
-        <Link to="/join" style={{margin: '3rem', fontSize:'20px'}}>Comunidad</Link>
-      </nav>
+      <header>
+          <div class="logo">
+              <a class="logolink" href="#"><img id="landing_logo" src="logo.png" alt="Logo"/></a>
+              <h1><a class="logolink" href="#">Site Name</a></h1>
+          </div>
+      </header>
+      <nav className='sidebar'>        
+        <Link to="/" className=''><i class="material-icons">home</i><span>Inicio</span></Link>
+        <Link to="/indicators" className=''><i class="material-icons">insert_chart</i><span>Indicadores</span></Link>
+        <Link to="/join" className='' ><i class="material-icons">settings</i><span>Comunidad</span></Link>
+      </nav>  
     </section>
   );
 };
