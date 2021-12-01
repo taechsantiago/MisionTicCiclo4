4dc29f38ce410caa69e76e18ba33ec9fd3725284
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-11-30 21:01:06 -0500

    Se agregan las media cards y se da mas forma al dashboard

--- ui-web/src/Pages/Dashboard.js
+++ ui-web/src/Pages/Dashboard.js
@@ -1,11 +1,22 @@
-
+import MediaCard from '../components/Dashboard/MediaCard';
 
 function Dashboard() {
-    return (
-      <div className="App">
-          Dashboard Page
-      </div>
-    );
-  }
-  
-  export default Dashboard;
+  return (
+    <div className="App">
+      <section style={{maxHeight:'200px'}}>
+        <img src="../images/320.png" alt="banner-aqi" />
+      </section>
+      <section style={{margin:'80px auto', width:'85%', textAlign:'justify', textJustify:'inter-word'}}>
+        <p>En Colombia, el monitoreo y control de la contaminación atmosférica ha tomado día a día mayor relevancia, debido a que, según cifras de la Organización Mundial de la Salud, una de cada ocho muertes ocurridas a nivel mundial, es ocasionada por la contaminación del aire. A nivel nacional, el Departamento Nacional de Planeación estimó que, durante el año 2015, los efectos de este fenómeno estuvieron asociados a 10.527 muertes y 67,8 millones de síntomas y enfermedades. Adicionalmente, los costos ambientales asociados a la contaminación atmosférica en Colombia, durante los últimos años se incrementaron pasando de 1,1% del PIB de 2009 ($5,7 billones de pesos) a 1,59% del PIB de 2014 ($12 billones de pesos) y del 1,93% del PIB en 2015 ($15.4 billones de pesos), lo cual pone en evidencia la necesidad de seguir implementando estrategias para controlar, evaluar y monitorear estas sustancias.</p>
+      </section>
+      <section style={{display:'flex', flexFlow: 'wrap', width: '700px'}}>
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+      </section>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ ui-web/src/components/Dashboard/MediaCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function MediaCard() {
+  return (
+    <Card sx={{ maxWidth: 300, margin: '1rem' }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://media.springernature.com/full/springer-cms/rest/v1/img/18893370/v1/height/320.png"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" style={{marginLeft:'12rem'}}>Ver más</Button>
+      </CardActions>
+    </Card>
+  );
+}
