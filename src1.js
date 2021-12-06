6cebcfc5b8d7098b41c93d97104121144c9084df
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-12-06 18:58:03 -0500

    Sprint 4 y 5 actualizados

--- src1.js
+++ src1.js
@@ -1,75 +1,16 @@
-const { json } = require('express');
-const express = require('express');
+const express = require("express");
 const app = express();
-
-let aqi_ranges = require('./aqi_ranges.json');
-
-app.use(express.urlencoded({extended: false}));
-app.use(express,json());
-
-app.get('/api/air-quality/aqi-ranges', (res, req)=>{
-    res.json(aqi_ranges);
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.get('/api/air-quality/aqi-ranges', (req, res)=>{
+    res.json(aqi_ranges)
 });
-
-app.post('/api/air-quality/aqi-ranges', (res, req)=>{
-    req.body({"etiqueta": "ejemplo", "de": 301, "hasta": 350 });
-    aqi_ranges.push({"etiqueta": "ejemplo", "de": 301, "hasta": 350 })
+app.post('/api/air-quality/aqi-ranges', (req, res)=>{
+    var rangos = req.body;
+    aqi_ranges.push(rangos);
+    res.json(aqi_ranges)
 });
-
-app.get('', (res, req)=>{
-    //limites
+app.get('/api/air-quality/limits', (req, res)=>{
+   res.json(limits)
 });
-
-/* global.rangosAQI = [
-    {etiqueta: 'excelente', de:0, hasta:30},
-    {etiqueta: 'bueno', de:31, hasta:50},
-    {etiqueta: 'moderado', de:51, hasta:100},
-    {etiqueta: 'bajo', de:101, hasta:150},
-    {etiqueta: 'insalubre', de:151, hasta:200},
-    {etiqueta: 'peligroso', de:201, hasta:300},
-];
-
-const porcentaje = (limite, valor) => {
-    if (limite > 0){
-        return (valor/limite)*100;
-    }
-
-    return 0;
-}
-
-const calcularPocentajes = async(valorPM10, 
-                            valorPM2punto5, 
-                            valorNO2, 
-                            valorCO, 
-                            valorS02) => {
-
-    let response = await fetch("https://misiontic2022upb.vercel.app/api/air-quality/limits");
-    let limits = await response.json();
-
-    const porcentajes = {
-        porcentajePM10: porcentaje(limits.limitePM10, valorPM10),
-        porcentajePM2punto5: porcentaje(limits.limitePM2punto5, valorPM2punto5), 
-        porcentajeNO2: porcentaje(limits.limiteNO2, valorNO2), 
-        porcentajeCO: porcentaje(limits.limiteCO, valorCO), 
-        porcentajeS02: porcentaje(limits.limiteS02, valorS02)
-    }
-
-    return porcentajes;
-}
-
-const registrarAQI = async(value)=>{
-    let indice = -1;
-    let response = await fetch("https://misiontic2022upb.vercel.app/api/air-quality/aqi-ranges");
-    let rangosAQI = await response.json();
-
-    for(let item of rangosAQI){
-        if(value >= item.de && value<= item.hasta){
-            return item.etiqueta     
-        }
-    }
-    return 'fuera_de_rango'
-}
-
-module.exports.registrarAQI = registrarAQI;
-module.exports.calcularPocentajes = calcularPocentajes; */
 module.exports = app;
