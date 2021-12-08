48e6f9f97bd68b98218d1eb882729eaf4ff2f788
brayan-cincoe <brayanrcaicedo@gmail.com>
2021-12-07 21:53:29 -0500

    Update src.js

--- src.js
+++ src.js
@@ -1,11 +1,18 @@
 const nombreMenorGas = async () =>{
     let response = await  fetch("https://misiontic2022upb.vercel.app/api/air-quality/limits");
     let limites = await response.json();
-    let gases = [limites.limitePM10, limites.limitePM2punto5, 
-                limites.limiteNO2, limites.limiteCO, limites.limiteS02];
-    const min = Math.min(...gases);
+    delete limites.limitePM10;
+    delete limites.limitePM2punto5;
 
-    return min
+    let gases = Object.keys(limites);
+    const min = Math.min(...Object.values(limites));
+
+    nombreMenor = gases.find(key => limites[key] === min);
+    nombreMenor = nombreMenor.substring(6, nombreMenor.length);
+
+    return nombreMenor;
 };
 
-console.log("Minimo: ", await nombreMenorGas());
+//console.log("Minimo: ", await nombreMenorGas());
+
+module.exports.nombreMenorGas = nombreMenorGas;
